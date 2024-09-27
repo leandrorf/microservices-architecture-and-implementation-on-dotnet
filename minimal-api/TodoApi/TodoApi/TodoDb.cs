23ae13e584cf6a72fda093fcfa09000b60f7a8fa
Leandro Rodrigues <leandro.rodrigues.fernandes@live.com>
2024-09-27 17:36:42 -0300

    Criado a minimal-api

---
+++ minimal-api/TodoApi/TodoApi/TodoDb.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi
+{
+    public class TodoDb : DbContext
+    {
+        public DbSet<TodoItem> Todos { get; set; }
+        public TodoDb( DbContextOptions<TodoDb> options )
+            : base( options )
+        {
+        }
+    }
+}
